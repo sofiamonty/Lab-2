3646e0edf37a2fe67de84b6ccfe7bcd637ef4ab2
sofiamonty <sofia.montaguti@metropolia.fi>
2025-04-11 10:30:13 +0300

    Update multiply1.h

--- multiply1.h
+++ multiply1.h
@@ -1,13 +1,28 @@
 #ifndef MULTIPLY1_H
 #define MULTIPLY1_H
+#include <cmath>
 
 //Multiply function with integer parameters - Part A
-int Multiply(int a, int b);
+int Multiply(int a, int b){
+    return a * b;
+}
 
 //Multiply function with double parameters - Part B
-double Multiply(double a, double b);
+double Multiply(double a, double b){
+    return a * b;
+}
 
 //Multiply function with integer, double and flag - Part C
-double Multiply(int a, double b, bool flag);
+double Multiply(int a, double b, bool flag){
+    double result = a * b;
+
+    if(!flag){
+       return result;
+    }
+    
+    else{
+        return ceil(result);
+    }
+}
 
 #endif
